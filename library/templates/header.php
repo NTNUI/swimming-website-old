6424ee1e6c4101bbc64e19b88252935afc1ba5f7
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-04-24 15:01:09 +0200

    fix styling in admin

--- library/templates/header.php
+++ library/templates/header.php
@@ -10,15 +10,15 @@ if ($title == "") $title = ucwords($frm_side);
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>NTNUI Svømming - <?php print $title; ?></title>
-	<LINK REL="SHORTCUT ICON" HREF="img/icons/logo.ico">
+	<LINK rel="SHORTCUT ICON" href="<?php print $base_url ?>/img/icons/logo.ico">
 	<?php /*
 	<link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet"> 
 	<link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
 	*/ ?>
-	<link rel="stylesheet" href="css/style.css" type="text/css"/>
-	<link rel="stylesheet" href="css/forms.css" type="text/css"/>
-	<link rel="stylesheet" href="css/store.css" type="text/css"/>
-	<link rel="stylesheet" href="css/smallscreen.css" type="text/css" media="screen and (max-width: 1080px)"/>
+	<link rel="stylesheet" href="<?php print $base_url ?>/css/style.css" type="text/css"/>
+	<link rel="stylesheet" href="<?php print $base_url ?>/css/forms.css" type="text/css"/>
+	<link rel="stylesheet" href="<?php print $base_url ?>/css/store.css" type="text/css"/>
+	<link rel="stylesheet" href="<?php print $base_url ?>/css/smallscreen.css" type="text/css" media="screen and (max-width: 1080px)"/>
 	<!-- Stripe fraud stuff reccomends this on all pages hmmm -->
 	<script src="https://js.stripe.com/v3/"></script>
 </head>
@@ -26,7 +26,7 @@ if ($title == "") $title = ucwords($frm_side);
 <body class="body_style">
 	<div class="maintable">
 		<div class="banner">
-			<a href="<?php print $t->get_url(""); ?>"><img src="img/icons/logo.jpg"?>"></a>
+			<a href="<?php print $t->get_url(""); ?>"><img src="<?php print $base_url ?>/img/icons/logo.jpg"?>"></a>
 		</div>
 		<div id="menu_show">Menu</span><span class="right"><a href="<?php ($language != "no" ? "" : "/en") . "/$frm_side"  ?>"><?php print ($language != "no" ? "Norsk" : "English"); ?></a></span></div>
 		<div id="meny_container">
