2b7eb86f0e7344cd084f2da37fd8d5a4c051cb43
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-06-04 15:04:05 +0200

    add function; log and quit/crash

--- library/util/log.php
+++ library/util/log.php
@@ -5,8 +5,12 @@
 // remember to pass __FILE__ and __LINE__ arguments
 // Don't rely on log messages to be ordered in log! They will be from different users.
 // Recomend putting it inside conditionals that will crash the site.
-function log_message($message, $file = __FILE__, $line=__LINE__){
+function log_message($message, $file = __FILE__, $line = __LINE__)
+{
     error_log(basename($file) . ":" . $line . " " . $message);
 }
-
-?>
+function log_exception($message, $file = __FILE__, $line = __LINE__)
+{
+    error_log(basename($file) . ":" . $line . " " . $message);
+    throw new Exception($message);
+}
