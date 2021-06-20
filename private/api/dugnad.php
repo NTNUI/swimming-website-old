60685b93d47e1ad1d2d6f8a66b51e0e509a38bbf
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-06-20 21:48:41 +0200

    refactor dugnad

--- private/api/dugnad.php
+++ private/api/dugnad.php
@@ -1,37 +1,60 @@
-<?php 
+<?php
 session_start();
 include_once("library/util/db.php");
 if (!$access_control->can_access("api", "dugnad")) {
 	header("HTTP/1.0 403 Forbidden");
-	die("You do not have access to this page");
+	log::die("Access denied", __FILE__, __LINE__);
 }
-function setDugnad($id, $value) {
+function setDugnad(int $id, bool $value)
+{
 	global $settings;
 	$conn = connect("medlem");
+	if (!$conn) {
+		log::die("Could not connect to database", __FILE__, __LINE__);
+	}
 	$sql = "UPDATE ${settings['memberTable']} SET `harUtførtFrivilligArbeid`=? WHERE ID=?";
 	$query = $conn->prepare($sql);
+	if (!$query) {
+		log::die("Could not prepare statement", __FILE__, __LINE__);
+	}
 	$query->bind_param("ii", $value, $id);
+	if (!$query) {
+		log::die("Could not bind params", __FILE__, __LINE__);
+	}
 	$result = $query->execute();
+	if (!$result) {
+		log::message("Query failed to execute", __FILE__, __LINE__);
+	}
 	$query->close();
 	$conn->close();
 	return $result;
 }
-if (isset($_GET["getRandom"]) && intval($_GET["getRandom"])) {
-	include_once("library/util/db.php");
 
+function getVolunteers(int $number)
+{
+	global $settings;
 	$conn = connect("medlem");
 
 	$sql = "SELECT id, fornavn, etternavn, phoneNumber, epost, `harUtførtFrivilligArbeid` FROM ${settings['memberTable']} WHERE (`harUtførtFrivilligArbeid` IS NULL OR `harUtførtFrivilligArbeid`=0) AND kontrolldato IS NOT NULL ORDER BY IFNULL(`harUtførtFrivilligArbeid`, 1) ASC, RAND() LIMIT ?";
 
 	$query = $conn->prepare($sql);
-	$query->bind_param("i", $_GET["getRandom"]);
-	if (!$query->execute()) {
-		print "error"; 
+	if (!$query) {
+		log::die("Failed to prepare query", __FILE__, __LINE__);
 	}
 
-	$query->bind_result($id, $first, $last, $phone, $email, $dugnad);
+	$query->bind_param("i", $number);
+	if (!$query) {
+		log::die("Failed to bind params", __FILE__, __LINE__);
+	}
+	if (!$query->execute()) {
+		log::die("Failed to execute query", __FILE__, __LINE__);
+	}
+
+	if (!$query->bind_result($id, $first, $last, $phone, $email, $dugnad)) {
+		log::die("failed to bind result", __FILE__, __LINE__);
+	}
 	$result = [];
-	while($query->fetch()) {
+	while ($query->fetch()) {
 		$result[] = array(
 			"id" => $id,
 			"name" => "$first $last",
@@ -40,36 +63,38 @@ if (isset($_GET["getRandom"]) && intval($_GET["getRandom"])) {
 			"dugnad" => $dugnad
 		);
 	}
+
 	$query->close();
 	$conn->close();
-	header("Content-type: application/json");
-	print json_encode($result);
-	return;
-} else if (isset($_GET["approve"]) && intval($_GET["approve"]) != 0) {
-	$access_control->log("api/dugnad", "approve", $_GET["approve"]);
-	$result = setDugnad($_GET["approve"], 1);
-	header("Content-type: application/json");
-	print json_encode(array("success" => $result));
-	return;
+	return $result;
+}
 
-} else if (isset($_GET["reject"]) && intval($_GET["reject"]) != 0) {
-	$access_control->log("api/dugnad", "reject", $_GET["reject"]);
-	$result = setDugnad($_GET["reject"], 0);
-	header("Content-type: application/json");
-	print json_encode(array("success" => $result));
-	return;
-
-} else if (isset($_GET["search"]) && strlen($_GET["search"]) > 0) {
+function search(string $name)
+{
+	// Search(string name)
+	global $settings;
 	$conn = connect("medlem");
 	$sql = "SELECT id, fornavn, etternavn, phoneNumber, epost, `harUtførtFrivilligArbeid` FROM ${settings['memberTable']} WHERE CONCAT(fornavn, ' ', etternavn) LIKE CONCAT('%', ?, '%')";
 
 	$query = $conn->prepare($sql);
-	$query->bind_param("s", $_GET["search"]);
+	if (!$query) {
+		log::die("Could not prepare statement", __FILE__, __LINE__);
+	}
+	$query->bind_param("s", $name);
+	if (!$query) {
+		log::die("Could not bind params", __FILE__, __LINE__);
+	}
 	$query->execute();
+	if (!$query) {
+		log::die("Could not execute query", __FILE__, __LINE__);
+	}
 
 	$query->bind_result($id, $first, $last, $phone, $email, $dugnad);
+	if (!$query) {
+		log::die("Could not bind results", __FILE__, __LINE__);
+	}
 	$result = [];
-	while($query->fetch()) {
+	while ($query->fetch()) {
 		$result[] = array(
 			"id" => $id,
 			"name" => "$first $last",
@@ -80,7 +105,54 @@ if (isset($_GET["getRandom"]) && intval($_GET["getRandom"])) {
 	}
 	$query->close();
 	$conn->close();
-	header("Content-type: application/json");
-	print json_encode($result);
-	return;
+	return $result;
 }
+
+$getRandom = argsURL("GET", "getRandom");
+$approve_id = argsURL("GET", "approve");
+$reject_id = argsURL("GET", "reject");
+$search = argsURL("GET", "search");
+
+// @return string|null
+function getAction(int|null $getRandom = 0, int|null $approve_id = 0, int|null $reject_id = 0, string|null $search = "")
+{
+	if ($getRandom) {
+		return "getRandom";
+	}
+	if ($approve_id) {
+		return "approve";
+	}
+	if ($reject_id) {
+		return "reject";
+	}
+	if ($search) {
+		return "search";
+	}
+	return null;
+}
+
+$action = getAction($getRandom, $approve_id, $reject_id, $search);
+switch ($action) {
+	case 'getRandom':
+		// don't log search queries
+		$result = getVolunteers($getRandom);
+		break;
+	case 'approve':
+		$access_control->log("api/dugnad", $action, $approve_id);
+		$result = setDugnad($approve_id, true);
+		break;
+	case 'reject':
+		$access_control->log("api/dugnad", $action, $reject_id);
+		$result = setDugnad($reject_id, false);
+		break;
+	case 'search':
+		// don't log search queries
+		$result = search($search);
+		break;
+
+	default:
+		return;
+}
+
+header("Content-type: application/json");
+print(json_encode($result));
