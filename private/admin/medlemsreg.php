7bff4b6f90b1f369e59259a51d7d89c43a74959d
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-06-20 22:41:08 +0200

    fix member approval

--- private/admin/medlemsreg.php
+++ private/admin/medlemsreg.php
@@ -1,125 +1,135 @@
 <style>
-.box {
-	padding: 0px;
-	margin: 0px;
-}
-button:disabled {
-	background-color: darkgray;
-	cursor: default;
-}
+	.box {
+		padding: 0px;
+		margin: 0px;
+	}
 
-h2 {
-	margin: 0;
-}
+	button:disabled {
+		background-color: darkgray;
+		cursor: default;
+	}
+
+	h2 {
+		margin: 0;
+	}
 </style>
 
-<h2>Nye medlemmer NTNUI Svømming <?php print date("Y")?></h2>
-Dette er kun medlemmer hvor kasserer ennå ikke har godkjent innbetaling eller lisens fra annen klubb.<br>
-<br>
+<div class="box">
 
-<?php
+	<h2>Nye medlemmer NTNUI Svømming <?php print date("Y") ?></h2>
+	Dette er kun medlemmer hvor kasserer ennå ikke har godkjent innbetaling eller lisens fra annen klubb.<br>
+	<br>
 
+	<?php
 
-function createTime($year, $month, $day) {
-	if ($year == 0 and $month == 0) {
-		if ($day == 0) return "I dag";
-		if ($day == 1) return "I går";
-	}
-	$time = "";
+	function createTime($year, $month, $day)
+	{
+		if ($year == 0 and $month == 0) {
+			if ($day == 0) return "I dag";
+			if ($day == 1) return "I går";
+		}
+		$time = "";
 
-	if ($year != 0) {
-		$time .= $year . " år ";
-	}
+		if ($year != 0) {
+			$time .= $year . " år ";
+		}
 
-	if ($month != 0) {
-		$time .= $month . ($month == 1 ? " månede " : " måneder");
-	}
+		if ($month != 0) {
+			$time .= $month . ($month == 1 ? " månede " : " måneder");
+		}
 
-	$time .= $day . ($day == 1 ? " dag" : " dager");
-	$time .= " siden";
-	return $time;
-}
-
-if ($_SESSION['logged_in'] == 1) {
-	$conn = connect("medlem");
-
-	// Check connection
-	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error());
+		$time .= $day . ($day == 1 ? " dag" : " dager");
+		$time .= " siden";
+		return $time;
 	}
 
-	$sql = "SELECT id, regdato, etternavn, fornavn, kommentar, epost, gammelKlubb, triatlon FROM ${settings['memberTable']} WHERE kontrolldato IS NULL OR YEAR(kontrolldato) <> DATE('y') ORDER BY id DESC";
+	if (Authenticator::is_logged_in()) {
+		global $settings;
+		$conn = connect("medlem");
 
-	$query = $conn->prepare($sql);
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
 
-	if (!$query->execute()) {
-	    die("Connection failed: ". $query->error);
+		$sql = "SELECT id, regdato, etternavn, fornavn, kommentar, epost, gammelKlubb, triatlon FROM ${settings['memberTable']} WHERE kontrolldato IS NULL OR YEAR(kontrolldato) <> DATE('y') ORDER BY id DESC";
+
+		$query = $conn->prepare($sql);
+
+		if (!$query->execute()) {
+			die("Connection failed: " . $query->error);
+		}
+
+		$query->bind_result($id, $regdato, $etternavn, $fornavn, $kommentar, $epost, $gammelKlubb, $triatlon);
+		while ($query->fetch()) {
+			$etternavn = htmlspecialchars($etternavn);
+			$fornavn = htmlspecialchars($fornavn);
+			$kommentar = htmlspecialchars($kommentar);
+			$epost = htmlspecialchars($epost);
+
+			$interval = date_diff(date_create(), date_create($regdato));
+			$tid = createTime($interval->y, $interval->m, $interval->d);
+	?>
+			<div class="box" id="medlem-<?php print $id ?>">
+				<h2><?php print "$etternavn, $fornavn" ?><button style="" onclick="godkjenn(<?php print $id ?>)">Godkjenn</button></h2>
+				<table class="center_table" style="width: 100%">
+					<tr>
+						<td>Registreringsdato:</td>
+						<td tooltip="test"><?php print $tid ?> (<?php print $regdato ?>)</td>
+					</tr>
+					<?php if ($gammelKlubb != "") { ?>
+						<tr>
+							<td>Lisens betalt av annen klubb:</td>
+							<td style="color: <?php $triatlon ? print "green" : print "red";?>">
+								<?php print $gammelKlubb ?>
+							</td>
+						</tr>
+					<?php } ?>
+					<?php if ($kommentar != "") { ?> <tr>
+							<td>Kommentar:</td>
+							<td><?php print $kommentar ?></td>
+						</tr><?php } ?>
+				</table>
+				<a class="btn" href="mailto:<?php print $epost ?>">Send epost</a>
+			</div>
+	<?php			}
+
+		$query->close();
+		mysqli_close($conn);
 	}
 
-	$query->bind_result($id, $regdato, $etternavn, $fornavn, $kommentar, $epost, $gammelKlubb, $triatlon);
-	while($query->fetch()) {
-		$etternavn = htmlspecialchars($etternavn);
-		$fornavn = htmlspecialchars($fornavn);
-		$kommentar = htmlspecialchars($kommentar);
-		$epost = htmlspecialchars($epost);
+	?>
+</div>
 
-		$interval = date_diff(date_create(), date_create($regdato));
-		$tid = createTime($interval->y, $interval->m, $interval->d);
-		?>
-			<div class="box" id="medlem-<?php print $id?>">
-			<h2><?php print "$etternavn, $fornavn"?><button style="" onclick="godkjenn(<?php print $id?>)" >Godkjenn</button></h2>
-			<table class="center_table" style="width: 100%">
-				<tr>
-					<td>Registreringsdato:</td>
-					<td tooltip="test"><?php print $tid ?> (<?php print $regdato?>)</td>
-				</tr>
-				<?php if ($gammelKlubb != "") { ?>
-				<tr>
-					<td>Lisens betalt av annen klubb:</td>
-					<td style="color: <?php if ($triatlon) { print "green"; } else { print "red"; } ?>"><?php print $gammelKlubb ?></td>
-				</tr>
-				<?php } ?>
-<?php if ($kommentar != "") { ?> 		<tr> 
-					<td>Kommentar:</td>
-					<td><?php print $kommentar ?></td>
-					</tr><?php } ?>
-			</table>
-			<a class="btn" href="mailto:<?php print $epost ?>">Send epost</a>
-		</div>
-<?php			}
-
-	$query->close();
-	mysqli_close($conn);
-}
-?>
 <script type="text/javascript">
-var getJSON = function(url, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url, true);
-	xhr.responseType = 'json';
-	xhr.onload = function() {
-		var status = xhr.status;
-		if (status === 200) {
-			callback(null, xhr.response);
-		} else {
-			callback(status, xhr.response);
-		}
+	var getJSON = function(url, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.responseType = 'json';
+		xhr.onload = function() {
+			var status = xhr.status;
+			if (status === 200) {
+				callback(null, xhr.response);
+			} else {
+				callback(status, xhr.response);
+			}
+		};
+		xhr.send();
 	};
-	xhr.send();
-};
 
-function godkjenn(id) {
-	let button = document.querySelector("#medlem-" + id).querySelector("button");
-	button.disabled = true;
-	button.innerText = "Godkjenner....";
-	getJSON("<?php print $base_url ?>/api/memberregister?id=" + id, function (err, json) {
-		if (err == null && json.success) {
-			button.disabled = true;
-			button.innerText = "Godkjent";
-		} else {
-			alert("Noe gikk galt: " + err);
-			button.innerText = "Feil!";
-		}
-	});
-}
+	function godkjenn(id) {
+		let button = document.querySelector("#medlem-" + id).querySelector("button");
+		button.disabled = true;
+		button.innerText = "Godkjenner....";
+		getJSON("<?php global $settings;
+					print $settings["baseurl"]; ?>/api/memberregister?id=" + id, function(err, json) {
+			if (err == null && json.success) {
+				button.disabled = true;
+				button.innerText = "Godkjent";
+			} else {
+				alert("Noe gikk galt: " + err);
+				button.innerText = "Feil!";
+			}
+		});
+	}
 </script>
