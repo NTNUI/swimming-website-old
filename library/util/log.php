a6c56690197fc818d151de5638cbcf7bb8b64e05
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-06-26 16:44:35 +0200

    add log::alert(), send mail to developer on crash

--- library/util/log.php
+++ library/util/log.php
@@ -1,22 +1,40 @@
 <?php
-
-// Usage: log::message("Something important happend", __FILE__, __LINE__);
+// TODO: Add ability to disable crash alerts
+// Usage: log::message("Something important happen", __FILE__, __LINE__);
 // log will be visible on the root of the directory in php.log (settings are in .htaccess file)
 // remember to pass __FILE__ and __LINE__ arguments
 // Don't rely on log messages to be ordered in log! They will be from different users.
-// Recomend putting it inside conditionals that will crash the site.
+// Recommend putting it inside conditionals that will crash the site.
 
-class log{
+class log
+{
 
+    // log event
     static function message($message, $file = __FILE__, $line = __LINE__)
     {
         error_log(basename($file) . ":" . $line . " " . $message);
     }
 
+    // Crash the server, write a log and alert the developer about the incident.
     static function die($message, $file = __FILE__, $line = __LINE__)
     {
+        global $settings;
         error_log(basename($file) . ":" . $line . " " . $message);
+        error_log(print_r(debug_backtrace(), true));
+        print("<script>alert('The site has crashed. The developers have now been informed and will resolve the issue as soon as possible. Sorry for the inconvenience.')</script>");
+        mail(
+            $settings["emails"]["developer"],
+            "Error on web server",
+            "Error occurred. Please check the logs. Have fun fixing bugs btw."
+        );
         throw new Exception($message);
     }
 
+    // Log and alert the user
+    static function alert($message, $file = __FILE__, $line = __LINE__)
+    {
+        $message = htmlspecialchars($message);
+        error_log(basename($file) . ":" . $line . " " . $message);
+        print("<script>alert('$message')</script>");
+    }
 }
