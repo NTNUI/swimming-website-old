806818840a2f69907d138f387cca70cfe18ad7aa
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-08-02 18:25:36 +0200

    fixed styling

--- library/templates/header.php
+++ library/templates/header.php
@@ -5,34 +5,5 @@ if ($title == "") $title = $t->get_translation($frm_side);
 if ($title == "") $title = ucwords($frm_side);
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>NTNUI Svømming - <?php print $title; ?></title>
-	<LINK rel="SHORTCUT ICON" href="<?php print $base_url ?>/img/icons/logo.ico">
-	<?php /*
-	<link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet"> 
-	<link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
-	*/ ?>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/style.css" type="text/css"/>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/forms.css" type="text/css"/>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/store.css" type="text/css"/>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/smallscreen.css" type="text/css" media="screen and (max-width: 1080px)"/>
-	<!-- Stripe fraud stuff reccomends this on all pages hmmm -->
-	<script src="https://js.stripe.com/v3/"></script>
-</head>
-
-<body class="body_style">
-	<div class="maintable">
-		<div class="banner">
-			<a href="<?php print $t->get_url(""); ?>"><img src="<?php print $base_url ?>/img/icons/logo.jpg"?>"></a>
-		</div>
-		<div id="menu_show">Menu</span><span class="right"><a href="<?php ($language != "no" ? "" : "/en") . "/$page"  ?>"><?php print ($language != "no" ? "Norsk" : "English"); ?></a></span></div>
-		<div id="menu_container">
-			<div id="menu" class="menu">
-				<?php include("library/templates/menu.php"); ?>
-			</div>
-		</div>
-		<div class="textboks">
+<html>	<div class="textboks">
 	
--- library/templates/menu.php
+++ library/templates/menu.php
@@ -2,17 +2,7 @@
 function menu_item($url, $name = "")
 {
 	global $t, $frm_side;
-	if ($name == "") $name = $url;
-	$link = $t->get_url($url);
-	$title = $t->get_translation($name, "menu");
-	$style = "";
-	if ($frm_side == $url) $style = " class='selected'";
-	print "<li><a href=\"$link\"$style>$title</a></li>\n";
-}
-?>
-<ul>
-	<?php
-	menu_item("mainpage");
+	if ($n
 	menu_item("practices");
 	menu_item("activities");
 	menu_item("enrollment");
