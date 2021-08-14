ec89c9df5ae3e1f59a3cd54241fd3dea0b4fadbb
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-08-14 10:00:48 +0200

    stop .htaccess tracking, fix background across env

--- library/templates/header.php
+++ library/templates/header.php
@@ -10,23 +10,29 @@ if ($title == "") $title = ucwords($frm_side);
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>NTNUI Svømming - <?php print $title; ?></title>
-	<LINK rel="SHORTCUT ICON" href="<?php print $base_url ?>/img/icons/logo.ico">
+	<link rel="shortcut icon" href="<?php global $settings; print($settings['base_url']); ?>/img/icons/logo.ico">
 	<?php /*
 	<link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet"> 
 	<link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
 	*/ ?>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/style.css" type="text/css"/>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/forms.css" type="text/css"/>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/store.css" type="text/css"/>
-	<link rel="stylesheet" href="<?php print $base_url ?>/css/smallscreen.css" type="text/css" media="screen and (max-width: 1080px)"/>
-	<!-- Stripe fraud stuff reccomends this on all pages hmmm -->
+	<link rel="stylesheet" href="<?php global $settings; print($settings['base_url']); ?>/css/style.css" type="text/css"/>
+	<link rel="stylesheet" href="<?php global $settings; print($settings['base_url']); ?>/css/forms.css" type="text/css"/>
+	<link rel="stylesheet" href="<?php global $settings; print($settings['base_url']); ?>/css/store.css" type="text/css"/>
+	<link rel="stylesheet" href="<?php global $settings; print($settings['base_url']); ?>/css/smallscreen.css" type="text/css" media="screen and (max-width: 1080px)"/>
+	<!-- Stripe fraud stuff recommends this on all pages hmmm -->
 	<script src="https://js.stripe.com/v3/"></script>
+	<style>
+		html {
+			background: url("<?php global $settings; print($settings['base_url']); ?>/img/background.jpg") no-repeat center center fixed;
+			background-size: cover;
+		}
+	</style>
 </head>
 
 <body class="body_style">
 	<div class="maintable">
 		<div class="banner">
-			<a href="<?php print $t->get_url(""); ?>"><img src="<?php print $base_url ?>/img/icons/logo.jpg"?>"></a>
+			<a href="<?php print $t->get_url(""); ?>"><img src="<?php global $settings; print($settings['base_url']); ?>/img/icons/logo.jpg"?>"></a>
 		</div>
 		<div id="menu_show">Menu</span><span class="right"><a href="<?php ($language != "no" ? "" : "/en") . "/$page"  ?>"><?php print ($language != "no" ? "Norsk" : "English"); ?></a></span></div>
 		<div id="menu_container">
