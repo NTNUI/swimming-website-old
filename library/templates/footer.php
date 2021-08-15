1c1a5aa325fb73b16730091f656362f1dc6e5135
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-08-15 20:22:06 +0200

    refactor json request

--- library/templates/footer.php
+++ library/templates/footer.php
@@ -1,6 +1,5 @@
 		</div>
 	</div>
-	<script src="<?php print $base_url; ?>/js/base.js"></script>
 	<div id="covidbox" style="position: fixed; background-color: red; width: 100%; bottom: 0; left: 0; text-align: center">
 		COVID-19: Please see updated guidelines, practical information and news on our facebook group: <a href="https://www.facebook.com/groups/2250060697">https://www.facebook.com/groups/2250060697</a>.<br>
 		You currently have to sign up <strong>before each practice</strong>.<br>
