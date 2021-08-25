8d46816cb6fda7809bd984b50fcafe657e2ca48e
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-08-26 01:13:10 +0200

    API serve json files, fix web store, translations

---
+++ library/util/api.php
@@ -0,0 +1,30 @@
+<?php
+
+define("HTTP_OK", 200);
+define("HTTP_INVALID_REQUEST", 400);
+define("HTTP_NOT_FOUND", 404);
+define("HTTP_NOT_IMPLEMENTED", 501);
+define("HTTP_INTERNAL_SERVER_ERROR", 500);
+
+
+class Response{
+    public int $code = HTTP_INTERNAL_SERVER_ERROR;
+    public array $data = ["error" => true, "message" => "no data"];
+    
+    public function send(){
+        header("Content-type: application/json");
+        http_response_code($this->code);
+        echo json_encode($this->data);
+    }
+
+    public function error(string $message, int $code = HTTP_INVALID_REQUEST){
+        $this->code = $code;
+        $this->data = ["error" => true, "message" => $message];
+    }
+
+    public function __construct()
+    {
+    }
+
+}
+?>
--- private/api/storelist.php
+++ private/api/storelist.php
@@ -7,8 +7,8 @@ foreach ($res as $i => $item) {
 	if (array_key_exists("image", $item)) {
 		$res[$i]["image"] = "$base_url/img/store/" . $item["image"];
 	}
-	$res[$i]["id"] = $item["api_id"];
-	unset($res[$i]["api_id"]);
+	$res[$i]["id"] = $item["item_hash"];
+	unset($res[$i]["item_hash"]);
 }
 
 header("Content-Type: application/json");
