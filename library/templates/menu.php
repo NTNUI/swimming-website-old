e57a475483c8864fbcf1fe75f399dc2379548ddc
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-11-16 19:36:47 +0100

    refactor styles
    Remove unused rules
    move menu to own file

--- library/templates/menu.php
+++ library/templates/menu.php
@@ -1,28 +1,26 @@
-<?php $t->load_translation("menu");
-function menu_item($url, $name = "")
+<?php $t->load_translation('menu');
+function menu_item($url, $name = '')
 {
-	global $t, $frm_side;
-	if ($name == "") $name = $url;
+	global $t;
+	if ($name == '') $name = $url;
 	$link = $t->get_url($url);
-	$title = $t->get_translation($name, "menu");
-	$style = "";
-	if ($frm_side == $url) $style = " class='selected'";
-	print "<li><a href=\"$link\"$style>$title</a></li>\n";
+	$title = $t->get_translation($name, 'menu');
+	print("<li class='menu-item'><a href=$link>$title</a></li>");
 }
 ?>
 <ul>
 	<?php
-	menu_item("mainpage");
-	menu_item("practices");
-	menu_item("activities");
-	menu_item("enrollment");
-	menu_item("board");
-	menu_item("FAQ");
-	menu_item("store");
+	menu_item('mainpage');
+	menu_item('practices');
+	menu_item('activities');
+	menu_item('enrollment');
+	menu_item('board');
+	menu_item('FAQ');
+	menu_item('store');
 	?>
-	<li id="lang_switch">
-		<a href="<?php print($base_url . ($language != "no" ? "" : "/en") . "/$frm_side"); ?>">
-			<?php print $t->get_translation("switchLanguage", "menu") ?>
+	<li id='lang_switch'>
+		<a href='<?php print($base_url . ($language != 'no' ? '' : '/en') . '/$frm_side'); ?>'>
+			<?php print $t->get_translation('switchLanguage', 'menu') ?>
 		</a>
 	</li>
 </ul>
