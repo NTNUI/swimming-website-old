c9d4e1c843a22d9cc4ce2bf7da237a07d495659e
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-11-16 18:59:45 +0100

    Update log.php
    code formating
    return json on error when using API

--- library/util/log.php
+++ library/util/log.php
@@ -16,32 +16,37 @@ class log
     }
 
     // Crash the server, write a log and alert the developer about the incident.
-    static function die($message, $file = __FILE__, $line = __LINE__)
+    static function die($message, $file = __FILE__, $line = __LINE__, bool $api = false)
     {
         global $settings;
-        header('HTTP/1.1 500 Internal Server Error');
+        http_response_code(500);
         error_log(basename($file) . ":" . $line . " " . $message);
         error_log(print_r(debug_backtrace(), true));
-        print("<script>alert('The site has crashed. The developers have now been informed and will resolve the issue as soon as possible. Sorry for the inconvenience.')</script>");
         mail(
             $settings["emails"]["developer"],
             "Error on web server",
             "Error occurred. Please check the logs. Have fun fixing bugs btw."
         );
+        if ($api) {
+            json_encode(["error" => "true"]);
+        } else {
+            print("<script>alert('The site has crashed. The developers have now been informed and will resolve the issue as soon as possible. Sorry for the inconvenience.')</script>");
+        }
         throw new Exception($message);
     }
 
     // return 400 Bad request to client and log the event
-    static function client_error($message = "Bad request", $file = __FILE__, $line= __LINE__){
-        header("HTTP/1.1 400 Bad Request");
+    static function client_error($message = "Bad request", $file = __FILE__, $line = __LINE__)
+    {
+        http_response_code(400);
         print($message);
-        error_log(basename($file) . ":" , $line, $message);
+        error_log(basename($file) . ":", $line, $message);
     }
 
     // Return 403 Forbidden to client and log the event
     static function forbidden($message = "Forbidden", $file = __FILE__, $line = __LINE__)
     {
-        header('HTTP/1.1 403 Forbidden');
+        http_response_code(403);
         print($message);
         error_log(basename($file) . ":" . $line . " " . $message);
         die();
