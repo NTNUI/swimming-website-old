1314e7bf4211aa565087966efb6a71b72422ef8f
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-12-08 23:43:53 +0100

    Add modal module

--- library/templates/modal.php
+++ library/templates/modal.php
@@ -1,9 +1,18 @@
+<?php
+global $settings;
+print("<link rel='stylesheet' type='text/css' href='${settings["baseurl"]}/css/modal.css'></link>");
+?>
+
 <template id="modal_template">
-    <div class="modal_background">
+    <div class="modal_background visible">
         <div class="modal_content">
             <h2 class="modal_header"></h2>
             <p class="modal_message"></p>
-            <button class="modal_button"></button>
+            <span class="status-graphic"></span>
+            <div class="bottom">
+                <button class="modal_button modal_accept_button"></button>
+                <button class="modal_button modal_decline_button"></button>
+            </div>
         </div>
     </div>
 </template>
