e9352bb8bdbaa8905d3ed7ad7a4d211614a384ff
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-12-08 21:49:07 +0100

    Refactor content pages

--- library/templates/header.php
+++ library/templates/header.php
@@ -12,26 +12,24 @@ if ($title == "") $title = ucwords($page);
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:title" content="NTNUI Svømming" />
+	<meta name="description" content="NTNUI Svømming er en svømmeklubb for studenter ved universitetet NTNU i Trondheim." />
+	<meta name="title" content="NTNUI Svømming" />
 	<title>NTNUI Svømming - <?php print $title; ?></title>
 	<link rel="shortcut icon" href="<?php print($settings['baseurl']); ?>/img/icons/logo.ico">
 	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/style.css" type="text/css" />
 	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/menu.css" type="text/css" />
-	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/board.css" type="text/css" />
-	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/enrollment.css" type="text/css" />
-	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/forms.css" type="text/css" />
-	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/store.css" type="text/css" />
+	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/content.css" type="text/css" />
+	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/inputs.css" type="text/css" />
 	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/admin.css" type="text/css" />
-	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/modal.css" type="text/css" />
 	<link rel="stylesheet" href="<?php print($settings['baseurl']); ?>/css/smallscreen.css" type="text/css" media="screen and (max-width: 1000px)" />
 	<!-- Stripe fraud stuff recommends this on all pages hmmm -->
 	<script src="https://js.stripe.com/v3/"></script>
-	<script>
-		<?php
+	<script><?php
 		if (isset($_SESSION['name'])) {
 			print("const NAME_USER = '" . $_SESSION['name'] . "';");
-		}
-		?>
 
+		}?>
 		const BASEURL = "<?php print $settings['baseurl']; ?>";
 		const STRIPE_PUBLISHABLE_KEY = "<?php print $settings['stripe']['publishable_key']; ?>";
 		const INVENTORY_URL = BASEURL + "/api/storelist?lang=" + "<?php global $language; print $language; ?>";
--- public/equipment.php
+++ public/equipment.php
@@ -1,28 +1,29 @@
 <?php
-function equipment_entry($title) {
-	global $base_url, $t ?>
-	<div class="box">
-		<h1><?php print $t->get_translation("${title}_header"); ?></h1>
-		<p><?php print $t->get_translation("${title}_content"); ?></p>
-	</div>
-<?php
+include_once("library/templates/content.php");
+global $t;
+print_content_header(
+	$t->get_translation("mainHeader"),
+	$t->get_translation("subHeader")
+);
+$entries = [
+	"briller",
+	"badebukse",
+	"badehette",
+	"plate",
+	"paddles",
+	"vannflaske",
+	"fotter",
+	"snorkel",
+	"pullbuoy"
+];
+
+foreach ($entries as $key) {
+	print_content_block(
+		$t->get_translation($key . "_header"),
+		$t->get_translation($key . "_description"),
+		"",
+		""
+	);
 }
-?>
-<div class="box">
-	<h1><?php print $t->get_translation("mainHeader"); ?></h1>
-	<p><?php print $t->get_translation("subHeader"); ?></p>
-	<p><?php print $t->get_translation("links"); ?></p>
-	<p><?php print $t->get_translation("contact"); ?></p>
-</div>
-<?php 
-equipment_entry("briller"); 
-equipment_entry("badebukse");
-equipment_entry("badehette");
-equipment_entry("plate");
-equipment_entry("paddles");
-equipment_entry("vannflaske");
-equipment_entry("fotter");
-equipment_entry("snorkel");
-equipment_entry("pullbuoy");
 
-
+style_and_script(__FILE__);
