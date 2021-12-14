56d4843523b3615b077f4e6e48d0e77d3cc9d729
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-12-14 13:11:10 +0100

    fix store product request

--- public/store.php
+++ public/store.php
@@ -27,73 +27,63 @@ print_content_header(
 		display_modal
 	} from "./js/modules/modal.js";
 
+	const requested_product_hash = "<?php isset($_REQUEST["product_hash"]) ? print $_REQUEST['product_hash'] : ""; ?>";
+
+	function handle_order(store, order) {
+		// on purchase click, display loading modal and send charge request to server
+		display_modal("Loading", "Attempting to empty your bank account", "", "", "wait");
+		store.charge(order.product, order.customer)
+			.then(async (serverResponse) => {
+				if (typeof(serverResponse === "object")) {
+					serverResponse = serverResponse.message;
+				}
+				await display_modal("Success", serverResponse, "Accept", "", "success");
+			}).catch((error) => {
+				if (typeof(error === "object")) {
+					error = error.message;
+				}
+				display_modal("Failure", error, "Accept", "", "failure");
+			});
+	}
+
 	const store = new Store(STRIPE_PUBLISHABLE_KEY, SERVER_TIME_OFFSET);
 	store.init(BASEURL + "/api/inventory").then(async () => {
 		const inventory = await store.inventory;
+		const requested_product = inventory.find(product => product.hash === requested_product_hash);
 
 		if (inventory.length) {
 			document.querySelector("#storeEmpty").classList.add("hidden");
 		}
 		for (let i = 0; i < inventory.length; ++i) {
+			// don't display hidden elements
+			if (!inventory[i].visibility) {
+				continue;
+			}
 			// Create a product entry
-			let productEntry = store.createProductEntry(inventory[i]);
-
-			let purchaseButtonHandler = () => {
-				// on buy click display checkout modal
+			const productEntry = store.createProductEntry(inventory[i]);
+			
+			const purchaseButtonHandler = () => {
+				// display checkout modal on purchase click
 				store.checkout(inventory[i])
-					.then((order) => {
-						// on purchase click, display loading modal and send charge request to server
-						display_modal("Loading", "Attempting to empty your bank account", "", "", "wait");
-						store.charge(order.product, order.customer)
-							.then(async (serverResponse) => {
-								if (typeof(serverResponse) === "object") {
-									serverResponse = serverResponse.message;
-								}
-								// wait until user closes the dialog
-								await display_modal("Success", serverResponse, "Accept", "", "success");
-							})
-							.catch((error) => {
-								if (typeof(error == "object")) {
-									error = error.message;
-								}
-								display_modal("Failure", error, "Accept", "", "failure");
-							});
-					});
+				.then((order) => {
+					handle_order(store, order);
+				});
 			}
 			productEntry.querySelector(".store_button").addEventListener("click", purchaseButtonHandler);
 			// append product entry to DOM
 			document.querySelector("#store_container").appendChild(productEntry);
 		}
-		<?php
-		if (isset($_REQUEST['product_hash'])) {
-		?>
-			// if some product is requested, display the purchase modal right away.
-			// TODO: refactor with code above.
-			const requested_product = inventory.find(product => product.hash == "<?php print $_REQUEST['product_hash'] ?>");
-			if (requested_product) {
-				store.checkout(requested_product)
-					.then((order) => {
-						// on purchase click, display loading modal and send charge request to server
-						display_modal("Loading", "Attempting to empty your bank account", "", "", "wait");
-						store.charge(order.product, order.customer).then(async (serverResponse) => {
-							if (typeof(serverResponse == "object")) {
-								serverResponse = serverResponse.message;
-							}
-							await display_modal("Success", serverResponse, "Accept", "", "success");
-						}).catch((error) => {
-							if (typeof(error == "object")) {
-								error = error.message;
-							}
-							display_modal("Failure", error, "Accept", "", "failure");
-						});
-					});
-			}
-		<?php
+
+		// if some product is requested, display the purchase modal right away.
+		if (requested_product) {
+			store.checkout(requested_product)
+				.then((order) => {
+					handle_order(store, order);
+				});
 		}
-		?>
 	});
 </script>
 <script src="https://js.stripe.com/v3/"></script>
-<?php 
+<?php
 style_and_script(__FILE__);
 ?>
