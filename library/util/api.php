9aa6719c0d03d9aae36c19fddbb4c90ee0ee0fd5
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-12-31 00:40:39 +0100

    Add support for ranged open period for enrollment

--- library/util/api.php
+++ library/util/api.php
@@ -2,6 +2,7 @@
 
 define("HTTP_OK", 200);
 define("HTTP_INVALID_REQUEST", 400);
+define("HTTP_FORBIDDEN", 403);
 define("HTTP_NOT_FOUND", 404);
 define("HTTP_NOT_IMPLEMENTED", 501);
 define("HTTP_INTERNAL_SERVER_ERROR", 500);
