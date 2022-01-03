d2b24f65684397d2cff46212a79f0d8adf793021
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-01-03 02:35:07 +0100

    Add footer

--- library/templates/footer.php
+++ library/templates/footer.php
@@ -1,3 +1,37 @@
         </div>
-    </body>
-</html>
+        <footer>
+            <div>
+                <?php
+                // Make image containers
+                $files = glob('img/sponsors/*.svg');
+                foreach ($files as $file) {
+                    $url = $settings["baseurl"] . "/" . $file;
+                    $name = basename($url, ".svg");
+                    $link = $t->get_translation("link_$name", "sponsors");
+
+                    print("<a rel='noreferrer noopener' target='_blank' href='$link'>");
+                    print("<img class='img-sponsor' src='$url' alt='$name'>");
+                    print("</a>");
+                }
+                ?>
+
+            </div>
+            <div>
+                <?php
+                    // TODO: generate this from db
+                ?>
+                <a rel="noreferrer noopener" target="_blank" href="https://ntnui.no">NTNUI</a>
+                <a rel="noreferrer noopener" target="_blank" href="https://github.com/ntnui">GitHub</a>
+                <a rel="noreferrer noopener" target="_blank" href="https://mediearkiv.ntnui.no/">Mediearkivet</a>
+                <a rel="noreferrer noopener" target="_blank" href="https://instagram.com/ntnuisvommegruppa/">Instagram</a>
+                <a rel="noreferrer noopener" target="_blank" href="https://www.facebook.com/NTNUISvomming">Facebook</a>
+            </div>
+            <div>
+                <p>
+                    © <?php print Date("Y"); ?> NTNUI
+                </p>
+        </div>
+        </footer>
+        </body>
+
+        </html>
